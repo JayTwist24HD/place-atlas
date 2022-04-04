68a22c4f1e6819e9bf3f1e9cf715dec805e7f7f5
JayTwist24HD <84015232+JayTwist24HD@users.noreply.github.com>
2022-04-05 06:55:00 +0700

    Update atlas.js
    Added Needy Streamer Overload

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 179, "name": "Needy Streamer Overload","description": "An icon from the game Needy Streamer Overload, a visual novel depicting daily life with “OMGkawaiiAngel-chan”, a young girl with a rather extreme need for approval attempting to become the #1 “Internet Angel”.","website": "https://store.steampowered.com/app/1451940/NEEDY_STREAMER_OVERLOAD/","subreddit": "https://www.reddit.com/r/NeedyStreamerOverload/","center": [1869.5,1263.5],"path": [[1856.5,1250.5],[1882.5,1250.5],[1882.5,1276.5],[1856.5,1276.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
